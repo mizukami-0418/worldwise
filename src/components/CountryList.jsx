b4c29aaa2ca5dc4a3931e91f6be5219e4ec31231
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-05-28 22:20:28 +0900

    機能追加: 国リストコンポーネントを追加し、Appコンポーネントにルーティングを統合

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,26 @@
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length) return <Message message="都市が見つかりません" />;
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
