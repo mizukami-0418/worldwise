ee571457b1f3c6e28e83b102c282088120d9a1fe
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-06-17 07:10:20 +0900

    機能追加: FakeAuthContextを新規作成し、認証状態の管理を実装。ログインとログアウト機能を提供。

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,68 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+    case "logout":
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function AuthProvider({ children }) {
+  const [{ isAuthenticated, user }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
